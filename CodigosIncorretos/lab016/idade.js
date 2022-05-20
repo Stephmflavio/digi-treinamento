fc8f2b802315d70843294b9a41967c40e042976a
Stephamflavio <Stephdpmello@gmail.com>
2022-05-20 11:32:17 -0300

    comentei os códigos dentro dos arquivos js, html e css

--- CodigosIncorretos/lab016/idade.js
+++ CodigosIncorretos/lab016/idade.js
@@ -1,11 +1,11 @@
 
-function calcidade () {
-    let agora = new Date
-    let ano = agora.getFullYear()
+// function calcidade () {
+//     let agora = new Date
+//     let ano = agora.getFullYear()
 
-    let nasc = Number(window.prompt('Em que ano você nasceu?'))
-    let idade = ano - nasc
+//     let nasc = Number(window.prompt('Em que ano você nasceu?'))
+//     let idade = ano - nasc
 
-    let saida = document.getElementById('saida')
-    saida.innerHTML = `<p>Quem nasceu em ${nasc} vai completar <strong>${idade}</strong> anos em ${ano}.</p>`
-}
+//     let saida = document.getElementById('saida')
+//     saida.innerHTML = `<p>Quem nasceu em ${nasc} vai completar <strong>${idade}</strong> anos em ${ano}.</p>`
+// }
